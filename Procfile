579b7f39f46e0d3dd16821fe09a4f415732ab3bd
Lucas Barbosa Laurindo <lucaslaurindo1107@gmail.com>
2024-11-04 22:01:49 -0300

    teste

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: dotnet restaurant.menu-app.api.dll
--- obj/Debug/net6.0/restaurant.menu-app.api.AssemblyInfo.cs
+++ obj/Debug/net6.0/restaurant.menu-app.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("restaurant.menu-app.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378c219f13216a19d5e162ce7397d5229cf3432e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b2d436ea0ebcb1ac0c408ed4a6860e10eb5ff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("restaurant.menu-app.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("restaurant.menu-app.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
